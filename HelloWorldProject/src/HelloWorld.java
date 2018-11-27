a3a593b1a68c02ee8dcd2372640489e25695e1d5
friedrichreichhartkk <45207672+friedrichreichhartkk@users.noreply.github.com>
2018-11-27 17:49:28 +0100

    Update HelloWorld.java

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hallo Klasse");
-		// das ist ein kommentar, kein code der ausgeführt wird.
+		System.out.println("Hallo Fritz");
+		// das ist ein kommentar, kein code der ausgefÃ¼hrt wird.
 		// das ist ein einzeiliger kommentar
 
 		/*
