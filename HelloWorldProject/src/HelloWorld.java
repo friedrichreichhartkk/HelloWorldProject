861f9cf0bd11e6b2c1462266fca03457e30391cb
freichhart <freichhart@KK-GYM-EDV1-01.gym.kk.local>
2018-11-27 17:39:52 +0100

    Kommentare hinzugefügt

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -2,9 +2,15 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hallo Klasse");
-		
+		// das ist ein kommentar, kein code der ausgeführt wird.
+		// das ist ein einzeiliger kommentar
+
+		/*
+		 * Mehrzeiligen Kommentar Schreiben
+		 */
+
 	}
 
 }
